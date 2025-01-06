2d18e8d065371a6f2255e5405a1efe300d9b4150
German Osin <german.osin@gmail.com>
2025-01-07 00:55:48 +0100

    Added repositories

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,10 @@
 plugins { `kotlin-dsl` }
 
+repositories {
+  mavenCentral()
+  gradlePluginPortal()
+}
+
 java {
   sourceCompatibility = JavaVersion.VERSION_17
   targetCompatibility = JavaVersion.VERSION_17
